d4e6d1a53a386f6ce4e9067eee958c36486a9242
Wilson Giese <giese.wilson@gmail.com>
2017-03-09 23:06:12 -0800

    Doesn't pass tests... not sure why yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+pub mod graphql; // synthesized by LALRPOP
+
 #[test]
-fn it_works() {
+fn test_IntValue() {
+    assert!(graphql::parse_IntValue("1").is_ok());
+    assert!(graphql::parse_IntValue("-1").is_ok());
+    assert!(graphql::parse_IntValue("123").is_ok());
+    assert!(graphql::parse_IntValue("-123").is_ok());
+}
+
+#[test]
+fn test_Comment() {
+    assert!(graphql::parse_Comment("# Hello, World\nX").is_ok());
+    assert!(graphql::parse_Comment("# Hello, World\r").is_ok());
+    assert!(graphql::parse_Comment("# Hello, World\r\n").is_ok());
+    assert!(graphql::parse_Comment("#a\tb\n").is_ok());
+    assert!(graphql::parse_Comment("#\n").is_ok());
+    assert!(graphql::parse_Comment("#   \n").is_ok());
+    assert!(graphql::parse_Comment("###\n").is_ok());
 }
