03fbecbd36c2e42d9d51e8032f256d7e6f5850f8
Wilson Giese <giese.wilson@gmail.com>
2017-03-14 00:24:50 -0700

    Messing around with custom lexer... looking for patterns to develop a parser generator

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,109 @@
-pub mod graphql; // synthesized by LALRPOP
+extern crate regex;
 
-#[test]
-fn test_IntValue() {
-    assert!(graphql::parse_IntValue("1").is_ok());
-    assert!(graphql::parse_IntValue("-1").is_ok());
-    assert!(graphql::parse_IntValue("123").is_ok());
-    assert!(graphql::parse_IntValue("-123").is_ok());
+use regex::Regex;
+
+pub struct Lexer {
+    source: Vec<char>,
+    position: usize,
 }
 
+impl Lexer {
+
+    pub fn new(s: &str) -> Lexer {
+        Lexer {
+            source: s.chars().collect(),
+            position: 0,
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<(), String> {
+        try!(self.accept_int());
+        try!(self.accept_end());
+        println!("Success!");
+        Ok(())
+    }
+
+    pub fn accept_int(&mut self) -> Result<(), String> {
+        if self.accept_negative_sign().is_ok() {
+            if self.is('0').is_ok() {
+                Ok(())
+            } else {
+                while self.accept_digit().is_ok() { }
+                Ok(())
+            }
+        } else {
+            try!(self.accept_nonzero_digit());
+            while self.accept_digit().is_ok() { }
+            Ok(())
+        }
+    }
+
+    pub fn accept_digit(&mut self) -> Result<(), String> {
+        let re = Regex::new(r"[0-9]").unwrap();
+
+        if self.position < self.source.len() {
+            if re.is_match(&mut self.source[self.position].to_string()) {
+                self.bump();
+                return Ok(());
+            }
+        } else {
+            return Err(format!("Expected digit, found EOF"));
+        }
+
+        Err(format!("Expected digit, found: {}", self.source[self.position]))
+    }
+
+    pub fn accept_nonzero_digit(&mut self) -> Result<(), String> {
+        let re = Regex::new(r"[1-9]").unwrap();
+
+        if self.position < self.source.len() {
+            if re.is_match(&mut self.source[self.position].to_string()) {
+                self.bump();
+                return Ok(());
+            }
+        } else {
+            return Err(format!("Expected non-zero digit, found EOF"));
+        }
+
+        Err(format!("Expected non-zero digit, found: {}", self.source[self.position]))
+    }
+
+    pub fn accept_negative_sign(&mut self) -> Result<(), String> {
+        self.is('-')
+    }
+
+    pub fn accept_end(&mut self) -> Result<(), String> {
+        self.is(';')
+    }
+
+    pub fn is(&mut self, c: char) -> Result<(), String> {
+
+        if self.position < self.source.len() {
+            if self.source[self.position] == c {
+                self.bump();
+                return Ok(());
+            }
+        } else {
+            return Err(format!("Expected {}, found EOF", c));
+        }
+
+        Err(format!("Expected {}, found: {}", c, self.source[self.position]))
+    }
+
+    fn bump(&mut self) {
+        self.position = self.position + 1;
+    }
+}
+
+
 #[test]
-fn test_Comment() {
-    assert!(graphql::parse_Comment("# Hello, World\nX").is_ok());
-    assert!(graphql::parse_Comment("# Hello, World\r").is_ok());
-    assert!(graphql::parse_Comment("# Hello, World\r\n").is_ok());
-    assert!(graphql::parse_Comment("#a\tb\n").is_ok());
-    assert!(graphql::parse_Comment("#\n").is_ok());
-    assert!(graphql::parse_Comment("#   \n").is_ok());
-    assert!(graphql::parse_Comment("###\n").is_ok());
+fn test() {
+    assert!(Lexer::new("1;").parse().is_ok());
+    assert!(Lexer::new("159;").parse().is_ok());
+    assert!(Lexer::new("-124;").parse().is_ok());
+    assert!(Lexer::new("-0;").parse().is_ok());
+    assert!(Lexer::new("123456789;").parse().is_ok());
+    assert!(Lexer::new("-01;").parse().is_err());
+    assert!(Lexer::new("abc;").parse().is_err());
+    assert!(Lexer::new("-zyx;").parse().is_err());
 }
